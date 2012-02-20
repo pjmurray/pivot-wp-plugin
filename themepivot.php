c916dbfe8891e7ec92ee62b8bb157f5fbae5f957
drew stewart <stewart.andrewjames@gmail.com>
2012-02-20 12:05:55 +1100

    muchos changes

--- themepivot.php
+++ themepivot.php
@@ -36,12 +36,6 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 class ThemePivot {
 	
-	private static $error = '';
-	private static $fp = null;
-	private static $dir = null;
-	private static $db_dump_filename = 'sql-out.txt';
-
-
 	/* #############################################
 	 *
 	 * ADMIN PAGE
@@ -95,9 +89,9 @@ class ThemePivot {
 	public static function plugin_scripts() {
 		wp_enqueue_script( 'themepivot_js' );
 		// embed the javascript file that makes the AJAX request
-		wp_enqueue_script( 'my-ajax-request', plugin_dir_url( __FILE__ ) . 'js/ajax.js', array( 'jquery' ) );
+		//wp_enqueue_script( 'my-ajax-request', plugin_dir_url( __FILE__ ) . 'js/ajax.js', array( 'jquery' ) );
 		// declare the URL to the file that handles th AJAX request (wp-admin/admin-ajax.php)
-		wp_localize_script( 'my-ajax-request', 'MyAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
+		//wp_localize_script( 'my-ajax-request', 'MyAjax', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
 	}
 
 	/*
@@ -128,12 +122,18 @@ class ThemePivot {
 	<?php
 
 		if( isset($_POST['submit']) && isset($_POST['job_id'])) {
-			$res = self::run($_POST['job_id']);
 			$t = $_POST['job_id'];
-			if ($res) {
+			if ( !is_wp_error ( $res = self::run( $_POST['job_id'] ) ) ) {
 				print "<div>";
 				print "<p>Your website has been uploaded to ThemePivot and has been made available to our marketplace of developers to start making your changes</p>";
-				print "<p><a href='http://www.themepivot.com/projects/$t' target='_blank'>Click here</a>&nbsp;to view your project page</p>";
+				print "<p><a href='http://pivot-market.herokuapp.com/projects/$t' target='_blank'>Click here</a>&nbsp;to view your project page</p>";
+				print "</div>";
+			} else {
+				// errored
+				$err_msg = $res->get_error_message();
+				print "<div>";
+				print "<p>Sorry, we've run into an error whilst taking a snapshot of your site</p>";
+				print "<p>$err_msg</p>";
 				print "</div>";
 			}
 		}
@@ -154,78 +154,25 @@ class ThemePivot {
 	/**
 	 * Runs the process
 	 *
-	 * @return void
 	 **/
 	public static function run( $job_id ) {
-		self::$error = new WP_Error();
 	
 		ThemePivot_Backup::instance()->set_job_id( $job_id );
-		ThemePivot_Backup::instance()->backup();
-
-		self::upload();
 		
-		if( is_wp_error( self::$error )) 
-			return self::$error;
-		else
+		if ( is_wp_error( $res = ThemePivot_Backup::instance()->backup() ) ) {
+			return $res;
+		}
+
+    /*
+		if ( is_wp_error( ThemePivot_Backup::instance()->upload() ) ) {
 			return false;
-	}
-
-	function can_user_backup() {
-
+		}
+		*/
+		return true;
 	}
 
 	function verify_nonce() {
 
-	}
-
-	/**
-	 * undocumented function
-	 *
-	 * @return void
-	 **/
-	/*
-	public static function json_posts()
-	{
-		// note - we might want to get specific post types if the theme is using them.
-		$args = array(
-			'posts_per_page' => 30,
-			'post_type'	=> array('post', 'page')
-		);
-		$query = new WP_Query( $args );
-		$json = json_encode( $query->posts );
-		$file =  ABSPATH . '/wp-content/uploads/themepivot/content.json';
-		$file = fopen( $file, 'w' ); // make self account for unopenable file
-		fwrite( $file, $json );
-		fclose( $file );	
-	}
-	*/
-	
-	/**
-	 * Upload zip to landing area 
-	 *
-	 * @return void
-	 **/
-	public static function upload() {
-		$svr = '107.21.227.54';
-		$conn_id = ftp_connect($svr);
-
-		$ftp_user = 'themepivot';
-		$ftp_pass = 'pivoting';
-		$login_res = ftp_login($conn_id, $ftp_user, $ftp_pass);
-		//ftp_pasv($conn_id, true);
-
-		if ((!$conn_id) || (!$login_res)) {
-			Throw new exception ('FTP connect failed');
-		}
-
-		$dest_file = 'uploads/' . ThemePivot_Backup::instance()->get_archive_filename();
-		$upload = ftp_put($conn_id, $dest_file, ThemePivot_Backup::instance()->get_archive_file(), FTP_BINARY);
-
-		if (!$upload) {
-			Throw new exception ('FTP upload failed');
-		}
-
-		ftp_close($conn_id);
 	}
 }
 
@@ -238,47 +185,51 @@ class ThemePivot_Backup {
 	// singleton
 	private static $instance;
 
-	// path to store archive file
+	// path to backup
+	private $wp_root_path;
+
+	// plugin path
+	private $plugin_path;
+
+	// archive path
 	private $archive_path;
 
-	// zip command path
-	private $zip_command;
+  // archive filename
+  private $archive_filename;
 
-	// path to backup
-	private $root;
-
-	// db conn
-	private $db;
-
-	// errors
-	private $errors;
-
-	// archive filename
+	// archive file
 	private $archive_file;
 
 	// sql dump filename
 	private $sql_dump_file;
 
-	// sql dump file handle
-	private $sql_dump_file_handle;
-
-	// manifest file
+	// project manifest file
 	private $manifest_file;
 
-	// private construct??
+	// sql dump file handle
+	private $sql_dump_fp;
+
+	// zip command path
+	private $zip_command;
+
+	// private constructor
 	private function __construct() {
 
-		@ini_set( 'memory_limi', apply_filters( 'admin_memory_limit', WP_MAX_MEMORY_LIMIT ) );
-		@set_time_limit( 0 );
+		// up the memory limit
+		//@ini_set( 'memory_limit', apply_filters( 'admin_memory_limit', WP_MAX_MEMORY_LIMIT ) );
+    @ini_set("memory_limit", "512M");
 
-		$this->root = $this->normalise_path( ABSPATH );
-		// need to look at replacing this with get_plugins_url('plugin name',__FILE__);
-		$this->archive_path = $this->normalise_path( WP_PLUGIN_DIR . '/themepivot/uploads' );
+		$this->wp_root_path = $this->normalise_path( ABSPATH );
+
+		$this->plugin_path = $this->normalise_path( dirname( __FILE__ ) );
+
+		$this->archive_path = $this->plugin_path  . '/uploads';
 		
-		$this->manifest_file = 'manifest.json';
-		//$this->sql_dump_file = DB_NAME . '.sql';
-		$this->sql_dump_file = 'wp_db_dump.sql';
-		//print "<div>path: $this->path</div>";
+		$this->manifest_file = $this->archive_path . '/manifest.json';
+		
+		$this->sql_dump_file = $this->archive_path . '/wp_db_dump.sql';
+		
+		//print "<div>archive_path: $this->archive_path</div>";
 	}
 
 	public static function instance() {
@@ -291,54 +242,54 @@ class ThemePivot_Backup {
 		return self::$instance;
 	}
 
-	// set job related params
-	public function set_job_id( $job_id ) {
-		$this->archive_file = $job_id . '.zip';
-	}
-
-	public function get_archive_filename() {
-		return $this->archive_file;
-	}
-
-	public function get_archive_file() {
-		return $this->normalise_path( $this->archive_path ) . '/' . $this->archive_file;
-	}
-
-	// normalise paths
-	public function normalise_path( $dir, $recursive = false ) {
-		
-		// replace slashes
-		$dir = str_replace( '//', '/', $dir );
-		$dir = str_replace( '\\', '/', $dir );
-
-		// remove trailing slash
-		$dir = untrailingslashit( $dir );
-
-		// recurse
-		if ( !$recursive && $this->normalise_path( $dir, true ) != $dir ) {
-			return $this->normalise_path( $dir );
-		}
-
-		return $dir;
-	}
-
 	public function backup() {
 
-		$this->init();
-		$this->generate_manifest();
-		$this->dump_database();
-		$this->archive();
+		// reset script execution time
+		@set_time_limit( 0 );
+
+		// initialise files etc
+		if ( is_wp_error( $res = $this->init() ) ) {
+			return $res;
+		}
+
+		// generate the project manifest
+		if ( is_wp_error( $res = $this->generate_manifest() ) ) {
+			$this->cleanup();
+			return $res;
+		}
+
+		// generate the wp database dump
+		if ( is_wp_error( $res = $this->dump_database() ) ) {
+			$this->cleanup();
+			return $res;
+		}
+
+		// archive wp assets
+		if ( is_wp_error( $res = $this->archive() ) ) {
+			$this->cleanup();
+			return $res;
+		}
+
+		// upload files
+
+		if ( is_wp_error( $res = $this->upload() ) ) {
+			$this->cleanup();
+			return $res;
+		}
+
+
+
+		// cleanup
+		//$this->cleanup();
+
+    return true;
 	}
 
 	private function init() {
 
-		$this->sql_dump_file_handle = $this->open_file( $this->archive_path . "/" . $this->sql_dump_file );
-		if ( false == $this->sql_dump_file_handle) {
-			// error
-			return false;
-		}
-
-		return true;
+		$this->sql_dump_fp = $this->open_file( $this->sql_dump_file );
+		
+		return $this->sql_dump_fp;
 	}
 
 	private function generate_manifest() {
@@ -346,6 +297,7 @@ class ThemePivot_Backup {
 		// extract options inc site_url, themepivot acct identifier etc
 	
 		// put options and job id into manifest
+		return true;
 	}
 
 	// dump wp database
@@ -355,19 +307,32 @@ class ThemePivot_Backup {
 
 		$tables = $wpdb->get_results( "SHOW TABLES", ARRAY_N );
 
-		$sql  =	"# " . __( 'ThemePivot WordPress Database Dump') . "\n" ;
+		// check for error
+		if ( false === $tables ) {
+			return new WP_Error("database_error", __( "Unable to read list of tables from database" ) );
+		}
+
+		// sql dump file header
+		$sql  =	"# " . __( "ThemePivot WordPress Database Dump") . "\n" ;
 		$sql .= "#\n";
-		$sql .= "# " . sprintf( __( 'Generated: %s' ), date( "l j. F Y H:i T" ) ) . "\n";
+		$sql .= "# " . sprintf( __( "Generated: %s" ), date( "l j. F Y H:i T" ) ) . "\n";
 		$sql .= "#\n";
 
 		foreach ( $tables as $table ) {
 
-			// set script execution time to 15 minutes for each table
-			@set_time_limit(15*60);
+			// reset script execution time
+			@set_time_limit( 0 );
 
-			$this->backup_table( $table[0], $sql );
-
+			// check for errors, kill execution if error returned
+			if ( is_wp_error( $res = $this->backup_table( $table[0], $sql ) ) ) {
+        $this->close_file( $this->sql_dump_fp );
+				return $res;
+			}	
 		}
+
+		$this->close_file( $this->sql_dump_fp );
+
+		return true;
 	}
 
 	/**
@@ -379,36 +344,62 @@ class ThemePivot_Backup {
 	 * to use the WordPress $wpdb object
 	 * @param string $table
 	 * @param string $segment
-	 * @return void
+	 * @return
 	 */
 	private function backup_table( $table, $sql) {
 		
 		global $wpdb;
+    print "<div>getting table $table</div>";
 
-		//print "<div>backing up table $table</div>";
+		$sql .= $this->drop_table_sql( $table );
 
-		$table_structure = $wpdb->get_results( "DESCRIBE $table" );
+		$sql .= $this->create_table_sql( $table );
 
-		if ( !$table_structure ) {
-			
-			$this->errors->add( "error", __( "Error getting table details" ) . ": $table" );
-			//print_r( $this->error->get_error_messages() );
-			return false;
-		}
-	
-		// Add SQL statement to drop existing table
-		$sql .= "\n";
+    $this->write_file( $this->sql_dump_fp, $sql );
+
+    // do a row count
+    $row_count = $wpdb->get_var( "SELECT COUNT(*) FROM $table" );
+
+    // guard against timeouts: if safe_mode is enabled skip tables that have 20K records
+    // and are not one of the core wp tables.
+    //if ( ( $row_count > 20000 ) && ( ini_get("safe_mode") ) ) {
+    // TODO: until timeout issue is addressed in the archive / upload fn just skip
+    // all large non core wp tables.
+    if ( ( $row_count > 20000 ) ) {
+      if ( ( strpos( $table, "comments" ) === false ) ||
+        ( strpos( $table, "posts" ) === false ) ) {
+        return null;
+      }
+    }
+
+		$sql = $this->table_data_sql( $table );
+
+		$sql .= $this->footer_sql( $table );
+		
+		$this->write_file( $this->sql_dump_fp, $sql );
+    print "<div>finished table $table</div>";
+	}
+
+	private function drop_table_sql( $table ) {
+
+		// drop existing table sql
+		$sql = "\n";
 		$sql .= "\n";
 		$sql .= "#\n";
 		$sql .= "# " . sprintf( __ ("Delete any existing table %s","wp-db-backup" ), $this->backquote( $table ) ) . "\n";
 		$sql .= "#\n";
 		$sql .= "\n";
 		$sql .= "DROP TABLE IF EXISTS " . $this->backquote( $table ) . ";\n";
-		
-		/* Table structure */
-		
-		// Comment in SQL-file
-		$sql .= "\n";
+
+		return $sql;
+	}
+
+	private function create_table_sql( $table ) {
+
+		global $wpdb;
+
+		// table struct header 
+		$sql = "\n";
 		$sql .= "\n";
 		$sql .= "#\n";
 		$sql .= "# " . sprintf( __ ( "Table structure of table %s" ), $this->backquote( $table ) ) . "\n";
@@ -417,20 +408,37 @@ class ThemePivot_Backup {
 		
 		$res = $wpdb->get_results("SHOW CREATE TABLE $table", ARRAY_N);
 
-		if ( $res ) {
+		// check for error
+		/*
+		if (false === $res) {
+			return new WP_Error( "database_error", __( "Unable to show create table statement for $table" ) );
+		} else {
 			$sql .= $res[0][1] . " ;";
 		}
+		*/
 
-		/* Table contents */
+		$sql .= $res[0][1] . " ;";
+	
+		return $sql;
+	}
 
-		// Comment in SQL-file
-		$sql .= "\n";
+	private function table_data_sql( $table ) {
+
+		global $wpdb;
+	
+		$table_structure = $wpdb->get_results( "DESCRIBE $table" );
+
+		// check for error
+		if ( false === $table_structure ) {
+			return new WP_Error( 'database_error', __( "Unable to describe table structure for $table" ) );
+		}
+
+		// data content header 
+		$sql = "\n";
 		$sql .= "\n";
 		$sql .= "#\n";
 		$sql .= "# " . sprintf( __( "Data contents of table %s"), $this->backquote( $table ) ) . "\n";
 		$sql .= "#\n";
-
-		$res = $wpdb->get_results( "SELECT * FROM $table", ARRAY_A);
 
 		$defs = array();
 		$ints = array();
@@ -455,10 +463,12 @@ class ThemePivot_Backup {
 		$batch = 100;
 
 		do {	
-			if ( !ini_get('safe_mode')) @set_time_limit(15*60);
 			$table_data = $wpdb->get_results("SELECT * FROM $table LIMIT {$current_row}, {$batch}", ARRAY_A);
 
-			if($table_data) {
+			// check for error
+			if (false === $table_data) {
+				return new WP_Error( "database_error", __( "Unable to retrieve rows for $table" ) );
+			} else {
 				foreach ($table_data as $row) {
 					$values = array();
 					foreach ($row as $key => $value) {
@@ -473,47 +483,60 @@ class ThemePivot_Backup {
 					}
 					$sql .= " \n" . $entries . implode(', ', $values) . ");";
 				}
+				
 				$current_row += $batch;
 
+				// reset script execution to prevent timeout
+				if ( ( $current_row % 10000 ) && !ini_get( "safe_mode" ) )
+					@set_time_limit( 0 );
+
 				// write out batch
-				$this->write_file( $this->sql_dump_file_handle, $sql );
+				$this->write_file( $this->sql_dump_fp, $sql );
 				$sql = '';
 			}
 		} while( ( count( $table_data ) > 0 ) );
-		
-		// Create footer/closing comment in SQL-file
-		$sql .= "\n";
+
+		return $sql;
+	}
+
+	private function footer_sql( $table ) {
+
+		// footer
+		$sql = "\n";
 		$sql .= "#\n";
 		$sql .= "# " . sprintf( __( "End of data contents of table %s"), $this->backquote( $table ) ) . "\n";
 		$sql .= "# --------------------------------------------------------\n";
 		$sql .= "\n";
 
-		$this->write_file( $this->sql_dump_file_handle, $sql );
+		return $sql;
 	}
 
 	private function open_file( $filename = '', $mode = 'w' ) {
 		if ( '' == $filename ) {
-			return false;
+			return new WP_Error( "file_error", __( "Filename cant be empty" ) );
 		}
 
-		$fp = @fopen( $filename, $mode );
-		return $fp;
+		if ( !$fp = @fopen( $filename, $mode ) ) {
+			return new WP_Error( "file_error", __( "Failed to open file $filename" ) );
+		}
+
+		return $fp; 
 	}
 
 	// param is a file handle
 	private function close_file( $fp ) {
-		if ( fclose( $fp ) )
-			return true;
+		if ( !fclose( $fp ) )
+			return new WP_Error("file_error", __( "Unable to close file $fp" ) );
 
-		return false;
+		return $fp;
 	}
 	
 	// param is a file handle
 	private function write_file( $fp, $data ) {
-		if ( fwrite( $fp, $data ) )
-			return true;
+		if ( !fwrite( $fp, $data ) )
+			return new WP_Error("file_error", __( "Unable to write file $fp" ) );
 		
-		return false;
+		return true;
 	}
 
 	/**
@@ -554,12 +577,6 @@ class ThemePivot_Backup {
 		}
 	}
 
-
-
-
-	/* ########################################
-	*/
-
 	private function archive() {
 
 		//$res = glob( ABSPATH . 'wp-content');
@@ -592,7 +609,7 @@ class ThemePivot_Backup {
 		}
 
 		// add db dump
-		//$files[] = $this->normalise_path( $this->archive_path ) . '/' . $this->sql_dump_file;
+		//$files[] = $this->normalise_path( $this->sql_dump_file;
 
 		//$res = $this->get_dir_iterative( ABSPATH, true );
 
@@ -633,7 +650,7 @@ class ThemePivot_Backup {
 
 		$this->load_pclzip();
 
-		$archive = new PclZip( $this->normalise_path( $this->archive_path ) . '/' . $this->archive_file );
+		$archive = new PclZip( $this->archive_file );
 		//$t = $this->normalise_path( $this->archive_path ) . '/' . $this->archive_file;
 		//print "<div>zip: $t</div>";
 
@@ -648,9 +665,10 @@ class ThemePivot_Backup {
 
 		$f = trim($f, ',');
 
-		$res = $archive->add($f, PCLZIP_OPT_REMOVE_PATH, ABSPATH );
+		$res = $archive->add( $f, PCLZIP_OPT_REMOVE_PATH, ABSPATH );
 
-		$res = $archive->add($this->normalise_path( $this->archive_path ) . '/' . $this->sql_dump_file, PCLZIP_OPT_REMOVE_PATH, $this->archive_path);
+		$res = $archive->add( $this->sql_dump_file, PCLZIP_OPT_REMOVE_PATH, $this->archive_path );
+
 		//print "<div>$t</div>";
 		//if ( !$res )
 			//print "<div>errored:  </div>";
@@ -681,7 +699,7 @@ class ThemePivot_Backup {
 
 		if ( defined( 'RecursiveDirectoryIterator::FOLLOW_SYMLINKS' ) ) {
 
-			$filesystem = new RecursiveIteratorIterator( new RecuriveDirectoryIterator( $this->normalise_path( $this->root ), RecursiveDirectoryIterator::FOLLOW_SYMLINKS ),
+			$filesystem = new RecursiveIteratorIterator( new RecuriveDirectoryIterator( $this->normalise_path( $this->wp_root_path ), RecursiveDirectoryIterator::FOLLOW_SYMLINKS ),
 				RecursiveIterator::SELF_FIRST, 
 				RecursiveIterator::CATCH_GET_CHILD );
 
@@ -694,7 +712,7 @@ class ThemePivot_Backup {
 					continue;
 				}
 
-				$pathname = str_ireplace( trailingslashit( $this->normalise_path( $this->root ) ), '', $this->normalise_path( $file->getPathname() ) );
+				$pathname = str_ireplace( trailingslashit( $this->normalise_path( $this->wp_root_path ) ), '', $this->normalise_path( $file->getPathname() ) );
 
 				// excludes
 				if ( $excludes && preg_match( '(' . $excludes . ')', $pathname ) )
@@ -708,11 +726,11 @@ class ThemePivot_Backup {
 			}
 
 		} else {
-			$this->files = $this->files_fallback( $this->normalise_path( $this->root ) );
+			$this->files = $this->files_fallback( $this->root_root );
 		}
 
 		// add db dump
-		$this->files[] = $this->normalise_path( $this->archive_path ) . '/' . $this->sql_dump_file;
+		$this->files[] = $this->sql_dump_file;
 
 		return $this->files;
 	}
@@ -721,7 +739,81 @@ class ThemePivot_Backup {
 
 	}
 
-	private function root() {
-		// return path
+	/* TRANSFER FUNCTIONS */
+
+	/**
+	 * Upload zip to landing area 
+	 *
+	 * @return void
+	 **/
+	public function upload() {
+
+    @set_time_limit( 0 );
+
+		$svr = '107.21.227.54';
+		$conn_id = ftp_connect($svr);
+
+		$ftp_user = 'themepivot';
+		$ftp_pass = 'pivoting';
+		$login_res = ftp_login($conn_id, $ftp_user, $ftp_pass);
+		//ftp_pasv($conn_id, true);
+
+		if ((!$conn_id) || (!$login_res)) {
+			return new WP_Error( "ftp_error", __( "FTP connect failed" ) );
+		}
+
+		$dest_file = 'uploads/' . $this->get_archive_filename();
+		$upload = ftp_put($conn_id, $dest_file, $this->get_archive_file(), FTP_BINARY);
+
+		if (!$upload) {
+			return new WP_Error( "ftp_error", __( "FTP upload failed" ) );
+		}
+
+		ftp_close($conn_id);
 	}
+
+	/* UTIL FUNCTIONS */
+
+	// set job id related params
+	public function set_job_id( $job_id ) {
+		$this->archive_filename = $job_id . '.zip';
+		$this->archive_file = $this->archive_path . '/' . $this->archive_filename;
+	}
+
+	// return the archive filename
+	public function get_archive_filename() {
+		return $this->archive_filename;
+	}
+
+	// return the absolute path to the archive file
+	public function get_archive_file() {
+		return $this->archive_file;
+	}
+
+	// normalise paths
+	public function normalise_path( $dir, $recursive = false ) {
+		
+		// replace slashes
+		$dir = str_replace( '//', '/', $dir );
+		$dir = str_replace( '\\', '/', $dir );
+
+		// remove trailing slash
+		$dir = untrailingslashit( $dir );
+
+		// recurse
+		if ( !$recursive && $this->normalise_path( $dir, true ) != $dir ) {
+			return $this->normalise_path( $dir );
+		}
+
+		return $dir;
+	}
+
+	private function cleanup() {
+
+		// remove generated files
+		@unlink($this->archive_file);
+		@unlink($this->sql_dump_file);
+		@unlink($this->manifest_file);
+	}
+
 } //end
